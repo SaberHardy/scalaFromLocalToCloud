486f890de834c8366900f928ce49cac3799a3f9f
Consultant <samirsamado5@gmail.com>
2023-12-05 12:00:35 -0500

    case class for table schema

---
+++ src/main/scala/com/exerices/SparkDataFrames/README.md
@@ -0,0 +1,28 @@
+> *Spark DataFrames*
+In Apache Spark, a DataFrame is a distributed collection of data,
+> organized into named columns. It is similar to a table in 
+> a relational database or a data frame in R or Python's pandas library. 
+> Spark DataFrames provide a programming interface 
+> for working with structured and semi-structured data.
+
+> Case Class In Scala
+
+* A case class is a regular class with some additional features.
+* It is primarily used in regular Scala programming and is not specific to Spark.
+
+_> A case class is a regular class with some additional features.
+
+_> It is primarily used in regular Scala programming and is not specific to Spark.
+
+> StructType (Spark SQL)
+
+* StructType is a part of Spark SQL and is used to define the schema of a DataFrame.
+* StructType is not focused on immutability. It is used to describe the structure of the data rather than instances of data.
+
+---------------------------------------------------------------------------
+## <p align="center"> Conclusion </p>
+* In summary, case classes are more geared towards defining instances 
+of structured data in a regular Scala program, while StructType 
+is used for defining the structure of data within the context of
+Spark SQL and DataFrames. Depending on your use case, you might 
+use one or both in a Spark application.
---
+++ src/main/scala/com/exerices/SparkDataFrames/sparkDataFrames.scala
@@ -0,0 +1,68 @@
+package com.exerices.SparkDataFrames
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+
+case class SchemaCountries(
+                            countryName: String,
+                            countryState: String,
+                            countryLanguage: String,
+                            countryCode: String)
+
+object sparkDataFrames {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("DataFrames").setMaster("local[*]")
+    val sparkContext = new SparkContext(sparkConf)
+    val sparkSession = SparkSession.builder.getOrCreate()
+    import sparkSession.implicits._
+
+    sparkContext.setLogLevel("Error")
+
+    val read_file = sparkContext.textFile("dataFiles/countries.txt")
+    val split_filedata = read_file.map(x => x.split(","))
+
+    val allData = split_filedata.map(x => SchemaCountries(x(0), x(1), x(2), x(3)))
+
+    val english_data = allData.filter(x => x.countryLanguage.contains("English"))
+
+    english_data.foreach(println)
+
+    println("---------- Convert to Dataframe ------------")
+    val english_dataframe = english_data.toDF()
+    english_dataframe.foreach(println)
+
+    println("---------- print Schema of Dataframe ------------")
+    english_dataframe.printSchema()
+
+    println("---------- Select Specific columns from Dataframe ------------")
+    val allColumns_data = english_dataframe.select("countryName", "countryState", "countryLanguage", "countryCode")
+    allColumns_data.show(false)
+
+    println("---------- Select Specific columns with filter condition ------------")
+
+    //    allColumns_data.filter("ColumnName == Specific condition")
+    allColumns_data.filter("countryCode== 'IND' ").show()
+    /*
+
+    Result
+    * ---------- Select Specific columns with filter condition ------------
+    +-----------------+------------+---------------+-----------+
+    |      countryName|countryState|countryLanguage|countryCode|
+    +-----------------+------------+---------------+-----------+
+    |Arunachal Pradesh|    Itanagar|        English|        IND|
+    |        Meghalaya|    Shillong|        English|        IND|
+    |         Nagaland|      Kohima|        English|        IND|
+    +-----------------+------------+---------------+-----------+
+
+    * */
+
+    /* Another method is creating a temp table and query using sql queries */
+
+    println("---------- Select using SQL condition ------------")
+    english_dataframe.createOrReplaceTempView("CountrySqlTable")
+    sparkSession.sql("select * from CountrySqlTable").show()
+
+
+  }
+
+}
