2989967f1c3e23128917bfdbf970cf6bb553cf17
Consultant <samirsamado5@gmail.com>
2023-12-15 11:53:58 -0500

    add aggregation

--- src/main/scala/com/exerices/StringFunctions/data_aggregation.scala
+++ src/main/scala/com/exerices/StringFunctions/data_aggregation.scala
@@ -1,5 +1,27 @@
 package com.exerices.StringFunctions
 
-object data_aggregation {
+import org.apache.spark.sql.{SparkSession, functions}
+import org.apache.spark.sql.functions._
+import org.apache.spark.{SparkConf, SparkContext}
 
+object data_aggregation {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("Apply Aggregations").setMaster("local[*]")
+    val sparkContext = new SparkContext(sparkConf)
+    val spark = SparkSession.builder().getOrCreate()
+
+    // Read the file
+    val file_content = spark.read.format("csv").option("header", true)
+      .load("dataFiles/bankTransaction/bank_transactions.csv")
+
+    val df = file_content.select("TransactionID", "CustLocation", "CustAccountBalance")
+
+    df.show()
+
+    println("These are the data gotten from the data itself")
+
+    df.groupBy("CustLocation").agg(functions.sum(col("CustAccountBalance"))).show()
+
+    spark.stop()
+  }
 }
