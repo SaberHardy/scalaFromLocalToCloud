f7ed5a4e1c3a488f5aac4b247b3178b04382cdd8
Consultant <samirsamado5@gmail.com>
2023-12-18 11:46:20 -0500

    left/right/outer/left_anto/left_semi

--- src/main/scala/com/exerices/joins/Joins.scala
+++ src/main/scala/com/exerices/joins/Joins.scala
@@ -28,10 +28,40 @@ object Joins {
     depart_df.persist()
 
     println("_______ Inner JOIN ________")
-    val join_dfs = employee_df.join(depart_df,
+    val inner_join_dfs = employee_df.join(depart_df,
       employee_df("DEPARTMENT_ID") === depart_df("Dno"), "inner")
 
-    join_dfs.show()
+    inner_join_dfs.show()
+
+    println("_______ Left JOIN ________")
+    val left_join_dfs = employee_df.join(depart_df,
+      employee_df("DEPARTMENT_ID") === depart_df("Dno"), "left")
+
+    left_join_dfs.show()
+
+    println("_______ Right JOIN ________")
+    val right_join_dfs = employee_df.join(depart_df,
+      employee_df("DEPARTMENT_ID") === depart_df("Dno"), "right")
+
+    right_join_dfs.show()
+
+    println("_______ Full outer JOIN ________")
+    val outer_join_dfs = employee_df.join(depart_df,
+      employee_df("DEPARTMENT_ID") === depart_df("Dno"), "outer")
+
+    outer_join_dfs.show()
+
+    println("_______ Left Anti JOIN ________")
+    val leftAnti_join_dfs = employee_df.join(depart_df,
+      employee_df("DEPARTMENT_ID") === depart_df("Dno"), "left_anto")
+
+    leftAnti_join_dfs.show()
+
+    println("_______ Left Semi JOIN ________")
+    val leftSemi_join_dfs = employee_df.join(depart_df,
+      employee_df("DEPARTMENT_ID") === depart_df("Dno"), "left_semi")
+
+    leftSemi_join_dfs.show()
 
 
   }
