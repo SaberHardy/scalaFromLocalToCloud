c52c6a993f91ca50f27258324ba267f679da7db2
Consultant <samirsamado5@gmail.com>
2023-12-24 14:07:27 -0500

    flatting the topping column

--- src/main/scala/com/exerices/WorkWithJSON/NestedUsingStruct.scala
+++ src/main/scala/com/exerices/WorkWithJSON/NestedUsingStruct.scala
@@ -2,7 +2,7 @@ package com.exerices.WorkWithJSON
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, struct}
+import org.apache.spark.sql.functions.{col, explode, struct}
 
 object NestedUsingStruct {
   def main(args: Array[String]): Unit = {
@@ -23,5 +23,27 @@ object NestedUsingStruct {
     json_data.persist()
     json_data.printSchema()
     json_data.show()
+
+    // Flatten  the data using explode function
+
+    val flatten_df = json_data.withColumn(
+      "topping",
+      explode(col("topping")))
+
+    println("Flatten Data by creating new  column")
+    flatten_df.printSchema()
+    flatten_df.show()
+
+    println("Flatten Data rename id of topping after structure the data")
+
+    val new_id_data = flatten_df.withColumn(
+      "topping_id", col("topping.id"))
+      .withColumn("topping_type", col("topping.type"))
+      .drop("topping")
+
+    json_data.printSchema()
+    new_id_data.show()
+
+
   }
 }
